6d30de6d29da6a9c0d31f32e37024f30972cce3d
Butsakon55 <38588225+Butsakon55@users.noreply.github.com>
2018-04-23 00:38:32 +0700

    list-bear

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props){
+    super(porps)
+    this.state={data:[]};
+  }
+  componentDidMount(){
+    axios.get('http://localhost:8000/api/bears')
+    .then (result => {
+     this.setState({data:result.data})
+    })
+   
+  }
   render() {
     return (
-      <div>Hello World</div>
+      
+      <div>{
+      bear.map(bear=><div  key ={bear.id}>{bear.name}</div>)
+      }</div>
     );
   }
 }
