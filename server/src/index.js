7386d07f32f9ec250eb6723de6b8c132f5d6f4b0
Butsakon55 <38588225+Butsakon55@users.noreply.github.com>
2018-04-26 23:11:11 +0700

    sever support deleting

--- server/src/index.js
+++ server/src/index.js
@@ -1,27 +1,31 @@
-var express = require('express');
-var app = express();
-var router = express.Router();
-var bodyParser = require('body-parser');
-var bears = [
-
-    {
-     id:1,name:"bud"
-    },{
-        id:2,name:"bud1"
-    }
-];
+var express = require('express'); 
+var app = express(); 
+var router = express.Router(); 
+var bodyParser = require('body-parser'); 
 var cors = require('cors');
-router.route('/bears')
-.get(function(req,res){
-    res.send(bears)
-})
-.post(function(req, res) {
-var bear = {};
-bear.name = req.body.name;
-bears.push(bear);
-res.json({ message: 'Bear created!' });
-});
-app.use(cors())
-// all of our routes will be prefixed with /api
-app.use('/api', bodyParser.json(), router);
+  
+var bears = [
+    { id: '1', name: 'Ple' },
+    { id: '2', name: 'Tum' }
+]; 
+  
+router.route('/bears') 
+    .get(function(req, res) {
+        res.send(bears);
+    })
+    .post(function(req, res) { 
+        var bear = {}; 
+        bear.name = req.body.name; 
+        bears.push(bear); 
+        res.json({ message: 'Bear created!' }); 
+    })
+router.route('/bears/:id')
+    .delete(function(req, res){
+        bears = bears.filter(b => b.id !== req.params.id)
+        res.json({ message: 'Bear deleted!' }); 
+    }) 
+
+app.use(cors());
+// all of our routes will be prefixed with /api 
+app.use('/api', bodyParser.json(), router); 
 app.listen(8000);
