ce3efdaf838d1b5fd7805c319dfb372925413c59
Butsakon55 <38588225+Butsakon55@users.noreply.github.com>
2018-04-27 11:22:09 +0700

    support-create-bear

--- server/src/index.js
+++ server/src/index.js
@@ -5,9 +5,10 @@ var bodyParser = require('body-parser');
 var cors = require('cors');
   
 var bears = [
-    { id: '1', name: 'Ple' },
-    { id: '2', name: 'Tum' }
+    { id: '1', name: 'bud' },
+    { id: '2', name: 'bud1' }
 ]; 
+var last_bear_id = 3;
   
 router.route('/bears') 
     .get(function(req, res) {
@@ -16,6 +17,7 @@ router.route('/bears')
     .post(function(req, res) { 
         var bear = {}; 
         bear.name = req.body.name; 
+        bear.id = "" + (last_bear_id++);
         bears.push(bear); 
         res.json({ message: 'Bear created!' }); 
     })
