92e1576100c320ac7d59dd172c3f95e39484582a
Butsakon55 <38588225+Butsakon55@users.noreply.github.com>
2018-04-27 11:28:54 +0700

    แก้ combine-reducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,51 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { deleteBear, createBear } from './actions';
+import axios from 'axios';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+let bearReducer = function(state = [], action){
+  if(action){
+    if(action.type == 'FETCH_BEAR'){
+      return action.payload
+    }
+  }
+  return state
+}
+let store = createStoreWithMiddleware(combineReducers({
+  bear: bearReducer
+}))
+
+let fetchBearActionCreator = function(){
+  return (dispatch) => {
+    axios.get('http://localhost:8000/api/bears')
+    .then(result => {
+      dispatch({type: 'FETCH_BEAR', payload: result.data})
+    })
+  }
+}
 
 class App extends Component {
   constructor(props){
     super(props)
-    this.state = {bearName: ''};
+    this.state = {data: []};
   }
-  onBearNameChanged = (e) => {
-    this.setState({bearName: e.target.value});
+  componentDidMount(){
+    store.subscribe(() => {
+      this.setState({data: store.getState().bear})
+    })
+    store.dispatch(fetchBearActionCreator());
   }
-  toCreateBear = () => {
-    this.props.createBear({name: this.state.bearName});
-    this.setState({bearName: ''});
-  }
-
   render() {
-    let bears = this.props.bear;
+    let bears = this.state.data;
     return (
       <div>
         {
-          bears.map(bear => (
-            <div key={bear.id}>
-              {bear.name}
-              <button onClick={() => this.props.deleteBear(bear.id)}>X</button>
-            </div>
-          ))
+          bears.map(bear => <div key={bear.id}>{bear.name}</div>)
         }
-        <input value={this.state.bearName} onChange={this.onBearNameChanged}/>
-        <button onClick={this.toCreateBear}>Add</button>
       </div>
     );
   }
 }
 
-let mapStateToProps = (state) => (
-  {bear: state.bear}
-)
-export default connect(mapStateToProps, {
-  deleteBear,
-  createBear,
-})(App);
+export default App;
